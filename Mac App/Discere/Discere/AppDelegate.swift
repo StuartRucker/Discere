e618c5e0aa970877f64f42a6c0faae7920131a26
Rohan Iyer <rohan.iyer1999@gmail.com>
2015-06-26 08:25:38 -0700

    Added Mac Project
    segmented directory by device

---
+++ Mac App/Discere/Discere/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Discere
+//
+//  Created by Rohan Iyer on 6/26/15.
+//  Copyright (c) 2015 rohan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Mac App/Discere/Discere/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Discere
+//
+//  Created by Rohan Iyer on 6/26/15.
+//  Copyright (c) 2015 rohan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
