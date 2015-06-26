0799728daeb1e19cd9da3e4c6da3acd84f87368a
Rohan Iyer <rohan.iyer1999@gmail.com>
2015-06-26 08:01:46 -0700

    More UI Changes
    Added styled text fields and buttons to form

--- script.js
+++ script.js
@@ -5,17 +5,20 @@ ref.on('child_added', function(childSnapshot, prevChildKey) {
     var name = childSnapshot.child("name").val()
     if (name != "") {
         //$('#mytable').append('<tr><td>' + 'name: ' + name + '</td></tr>').fadeIn(1000);
-        $('<tr><td style: "{opacity: 0;}">' + 'name: ' + name + '</td></tr>').prependTo('#mytable').fadeOut(0).fadeIn(500);
+        $('<tr><td style: "{opacity: 0;}">' + ' ' + name + '</td></tr>').appendTo('#mytable').fadeOut(0).fadeIn(500);
     }
-    
+
     // code to handle new child.
     //alert("lmao")
 });
 
 function handleClick() {
     var childRef = ref.push()
-    childRef.set({
-        'name': document.getElementById("nameInput").value
-    })
+    if (document.getElementById("firstNameInput").value != "" || document.getElementById("lastNameInput").value != "") {
+        childRef.set({
+            'name': document.getElementById("firstNameInput").value + " " + document.getElementById("lastNameInput").value,
+            'class': document.getElementById("classInput").value
+        })
+    }
 
 }
