ad6ef725156f7f215a075561a107ab4a863a1dc8
Rohan Iyer <rohan.iyer1999@gmail.com>
2015-06-29 07:44:14 -0700

    Added form clearing on js and html files

--- Web App/script.js
+++ Web App/script.js
@@ -4,7 +4,6 @@ var ref = new Firebase("https://discere.firebaseio.com")
 ref.on('child_added', function(childSnapshot, prevChildKey) {
     var name = childSnapshot.child("name").val()
     var class1 = childSnapshot.child("class").val()
-    alert(class1)
     if (name != "") {
         //$('#mytable').append('<tr><td>' + 'name: ' + name + '</td></tr>').fadeIn(1000);
         $('<tr><td style: "{opacity: 0;}">' + 'name: ' + name + ', class:' + class1 + '</td></tr>').appendTo('#mytable').fadeOut(0).fadeIn(500);
@@ -21,6 +20,8 @@ function handleClick() {
             'name': document.getElementById("firstNameInput").value + " " + document.getElementById("lastNameInput").value,
             'class': document.getElementById("classInput").value
         })
+        var form = document.getElementById("myForm");
+        form.reset()
     }
 
 }
